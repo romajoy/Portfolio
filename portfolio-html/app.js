86f1cd85b7f89727bfc202f2ecb994d5224f3f4f
Roma Joy <joynormahouser@gmail.com>
2022-07-27 12:44:41 -0500

    july 27 commit

--- portfolio-html/app.js
+++ portfolio-html/app.js
@@ -1,36 +1,67 @@
 $(() => {
 
-    // const $flexbox = $('.nav').attr('id','flexbox')
-    // $('body').append($flexbox)
+// Elements Created for JQuery
 
+$aboutMeDiv = $('#bio-paragraph')//.hide(5000)
+$resumeDiv = $('#resume-paragraph')//.hide(5000)
+$projectsDiv = $('#projects-divy')//.hide(5000)
+$projectParas = $('.project-description')
+$projectA = $('#project1')
+$projectB = $('#project2')
+$projectC = $('#project3')
+$projectHeads = $('.head')
 
-    $aboutMeDiv = $('#about-div').hide(5000)
-    $resumeDiv = $('#resume-div').hide(5000)
-    $projectsDiv = $('#projects-div').hide(5000)
+// On Load, show/hide:
+$aboutMeDiv.show()
+$resumeDiv.hide()
+$projectsDiv.hide()
 
+// Funtions for on-click:
 const toggleAboutMe = () => {
-    $aboutMeDiv.show(4000)
-    $resumeDiv.hide()
-    $projectsDiv.hide()
+    $aboutMeDiv.toggle(4000)
+    $resumeDiv.hide(4000)
+    $projectsDiv.hide(4000)
 }
-
 const toggleResume = () => {
-    $resumeDiv.show(4000)
-    $aboutMeDiv.hide()
-    $projectsDiv.hide()
+    $resumeDiv.toggle(4000)
+    $aboutMeDiv.hide(4000)
+    $projectsDiv.hide(4000)
 }
-
 const toggleProjects = () => {
-    $projectsDiv.show(4000)
-    $aboutMeDiv.hide()
-    $resumeDiv.hide()
+    $projectsDiv.toggle(4000)
+    $aboutMeDiv.hide(4000)
+    $resumeDiv.hide(4000)
+    $projectParas.hide()
+    $projectHeads.show(4000)
+}
+const projectAx = () => {
+    $projectA.toggle(3000)
+    $projectB.hide(3000)
+    $projectC.hide(3000)
+}
+const projectBx = () => {
+    $projectB.toggle(3000)
+    $projectA.hide(3000)
+    $proejctC.hide(3000)
+}
+const projectCx = () => {
+    $projectC.toggle(3000)
+    $projectB.hide(3000)
+    $projectA.hide(3000)
 }
 
-    $('#about-me').on('click', toggleAboutMe)
-    $('#resume').on('click', toggleResume)
-    $('#projects').on('click', toggleProjects)
 
-const $flexboxfooter = $('<footer>').attr('id','flexbox')
-$('body').append($flexboxfooter)
+// OnClick Actions
+    $('#about-header').on('click', toggleAboutMe)
+    $('#resume-header').on('click', toggleResume)
+    $('#projects-header').on('click', toggleProjects)
+    $('#p1-head').on('click', projectAx)
+    $('#p2-head').on('click', projectBx)
+    $('#p3-head').on('click', projectCx)
+
+
+// flexbox variable, possibly not needed
+// const $flexboxfooter = $('<footer>').attr('id','flexbox')
+// $('body').append($flexboxfooter)
 
 })
